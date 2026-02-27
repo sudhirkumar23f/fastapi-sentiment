b51e8df6549ee4391bb5f82a17be06d93e6824ef
sudhirkumar3762 <23f1002787@ds.study.iitm.ac.in>
2026-02-27 03:06:47 +0000

    Enable CORS

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CommentRequest(BaseModel):
     comment: str
